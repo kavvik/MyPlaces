123d2f988e99ec1c3bf65cefda6f18932a69e361
Arten Kozukin <kavvik@icloud.com>
2022-11-22 02:26:51 +0100

    custom table view cell

---
+++ MyPlace/CustomViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomViewCell.swift
+//  MyPlace
+//
+//  Created by a.koziukin on 22.11.2022.
+//
+
+import UIKit
+
+class CustomViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    
+    
+}
